23601497c9e93bd2acf76aff403ce608ee1ce86c
Imri <imribalourd@gmail.com>
2019-09-28 20:26:30 +0200

    commit

--- public/build/app.a1493c86.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["app"],{ldto:function(n,o,s){},ng4s:function(n,o,s){"use strict";s.r(o);s("sZ/o"),s("ldto");console.log("Hello Webpack Encore! Edit me in assets/js/app.js")},"sZ/o":function(n,o,s){}},[["ng4s","runtime"]]]);
